18fc9a4463752c3ff50b8637eac20901c5f780f4
Rey <reyvienepasiculan7@gmail.com>
2022-03-10 22:02:51 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "Birthday.mp4");
+  ifrm.setAttribute("src", "https://youtu.be/ifTIu_YzvHs");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
